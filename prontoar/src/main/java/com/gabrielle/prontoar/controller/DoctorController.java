bd3418cbc595e5181dae59b3d34363c84f4790bd
Gabrielle Rodrigues <73626380+gabrielle-1@users.noreply.github.com>
2022-11-08 09:17:33 -0300

    Changing a Doctors endpoint

--- prontoar/src/main/java/com/gabrielle/prontoar/controller/DoctorController.java
+++ prontoar/src/main/java/com/gabrielle/prontoar/controller/DoctorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/doctor")
+@RequestMapping("api/v1/doctors")
 @Slf4j
 @AllArgsConstructor
 public class DoctorController {
